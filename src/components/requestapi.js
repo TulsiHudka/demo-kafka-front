7406d83d5393fd4de77d0f21cb213b67f5d71fd7
TulsiHudka <tulsi.hudka@aspiresoftserv.in>
2023-06-30 13:31:13 +0530

    socket done

--- src/components/requestapi.js
+++ src/components/requestapi.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
 
 const RequestApi = () => {
-    const userId = "a43c9b0d-7380-41e2-847a-d11eaa5e7325"; // Replace with your hardcoded user ID
+    // const [process_Notification, setProcess_Notification] = useState("")
+    const user_id = "a43c9b0d-7380-41e2-847a-d11eaa5e7325";
     useEffect(() => {
-        handleButtonClick();
 
         const socket = io('http://localhost:8000');
 
@@ -13,16 +13,20 @@ const RequestApi = () => {
             console.log('WebSocket connection established');
         });
 
-        socket.on('notification', (message) => {
-            console.log(message);
-            console.log('Received notification:', message);
+        socket.on('notification', () => {
+            console.log('notification received');
+        });
+
+        socket.on('processNotification', (processNotification) => {
+            console.log('Received notification:', processNotification);
+            // setProcess_Notification(processNotification)
+            // alert(process_Notification)
             // Handle the notification message
         });
 
-        socket.on('result', (result) => {
-            // Handle the received result
-            console.log('Received result:', result);
-            // Update your component state or perform other operations with the result
+        socket.on('response', (response) => {
+            // Handle the received respponse
+            console.log('Received response:', response);
         });
 
         // socket.on('disconnect', () => {
@@ -32,21 +36,22 @@ const RequestApi = () => {
         // return () => {
         //     socket.disconnect();
         // };
+        handleButtonClick();
     }, []);
 
 
     const handleButtonClick = async () => {
         try {
-            console.log(userId)
-            const response = await axios.get('http://localhost:8000/request', {
+            console.log(user_id)
+            const response = await axios.get('http://localhost:8000/api/request', {
                 headers: {
-                    'userId': userId
+                    user_id
                 }
             });
             // console.log("hello");
             console.log('Node API GET request successful');
             // Process the response data here
-            console.log(response.data);
+            console.log(response);
         } catch (error) {
             // Handle error
             console.error('Error making Node API GET request:', error);
@@ -56,6 +61,7 @@ const RequestApi = () => {
     return (
         <div>
             <button onClick={handleButtonClick}>Make Request</button>
+            {/* <h5>response: {response}</h5> */}
         </div>
     );
 };
